14dc477e499e94cb70e01bb4aaeecd0cf355b794
Andrea Mazzoleni <amadvance@gmail.com>
2014-03-05 21:59:23 +0100

    Allows a fast build with -f.

--- makedist.sh
+++ makedist.sh
@@ -7,7 +7,13 @@ if ! ./configure.windows-x86; then
 	exit 1
 fi
 
-if ! make check distwindows distclean; then
+if ! test "x$1" = "x-f"; then
+	if ! make check; then
+		exit 1
+	fi
+fi
+
+if ! make distwindows distclean; then
 	exit 1
 fi
 
@@ -23,7 +29,13 @@ if ! ./configure ; then
 	exit 1
 fi
 
-if ! make distcheck; then
+if ! test "x$1" = "x-f"; then
+	if ! make check; then
+		exit 1
+	fi
+fi
+
+if ! make dist; then
 	exit 1
 fi
 
