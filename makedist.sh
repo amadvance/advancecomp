17d294c4142ebf8e5792c7a963b08880f8d07576
Andrea Mazzoleni <amadvance@gmail.com>
2017-01-15 15:17:20 +0100

    Add Windows 64 bit distribution

--- makedist.sh
+++ makedist.sh
@@ -13,7 +13,15 @@ if ! test "x$1" = "x-f"; then
 	fi
 fi
 
-if ! make distwindows distclean; then
+if ! make distwindows-x86 distclean; then
+	exit 1
+fi
+
+if ! ./configure.windows-x64; then
+	exit 1
+fi
+
+if ! make distwindows-x64 distclean; then
 	exit 1
 fi
 
