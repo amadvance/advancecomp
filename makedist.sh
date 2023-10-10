d1d79d814e5b3bb19ca9d81ce5599895467e1d7c
Andrea Mazzoleni <amadvance@gmail.com>
2023-10-10 17:20:24 +0200

    Test the 64 bits Windows version instead of the 32 bits one

--- makedist.sh
+++ makedist.sh
@@ -10,18 +10,18 @@ if ! ./configure.windows-x86; then
 	exit 1
 fi
 
-if ! test "x$1" = "x-f"; then
-	if ! make check; then
-		exit 1
-	fi
-fi
-
 if ! make distwindows-x86 distclean; then
 	exit 1
 fi
 
 if ! ./configure.windows-x64; then
 	exit 1
+fi
+
+if ! test "x$1" = "x-f"; then
+	if ! make check; then
+		exit 1
+	fi
 fi
 
 if ! make distwindows-x64 distclean; then
