82792d7af93c213d7e91b42d6c5212575bf81957
Andrea Mazzoleni <amadvance@gmail.com>
2024-03-09 17:47:13 +0100

    Use the modern autoreconf in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 #
-echo "Generating build information using aclocal, automake and autoconf"
-echo "This may take a while ..."
+echo "Generating build information using autoreconf"
 
-# Touch the timestamps on all the files since CVS messes them up
-touch configure.ac
-
-# Regenerate configuration files
-aclocal
-automake --add-missing --force-missing
-autoconf
-autoheader && touch config.h.in
+# All is done by autoreconf
+autoreconf -f -i
 
 # Run configure for this platform
 echo "Now you are ready to run ./configure"
+
